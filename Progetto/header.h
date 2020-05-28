8ee72b791bdc77760b99c0074dd5fb462502e4ff
emanuele53 <ep.abc00@gmail.com>
2020-05-28 17:24:13 +0200

    percorsi minimi

--- Progetto/header.h
+++ Progetto/header.h
@@ -1,13 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<ctype.h>
 
 
-typedef struct prenotazioni{
+typedef struct prenotazioni
+{
     char citta_partenza[20];
     char citta_arrivo[20];
     struct prenotazioni* next;
-}prenotazioni;
+} prenotazioni;
 
 typedef struct arco
 {
@@ -20,6 +22,7 @@ typedef struct arco
 typedef struct vertice
 {
     char citta[30];
+    int indice_vertice;
     struct vertice*next_vertice; // puntatore al prossimo vertice
     struct arco*next_arco;
 } vertice;
@@ -29,6 +32,7 @@ typedef struct grafo
     int nv;        // numero vertici
     vertice*lista; // puntatore al primo vertice
 } grafo;
+//*******************************************************************************
 
 
 void errore(char messaggio[50])
@@ -39,6 +43,7 @@ void errore(char messaggio[50])
     exit(0);
 }
 
+//*******************************************************************************
 
 grafo*nuovo_grafo()  // si alloca un nuovo grafo, senza vertici e senza archi
 {
@@ -75,7 +80,6 @@ void stampa_grafo(grafo*g)
 
 
 //************************************** gestione liste *****************************
-
 //************** vertice *************************
 vertice* crea_vertice(char citta[30])
 {
@@ -86,6 +90,7 @@ vertice* crea_vertice(char citta[30])
     {
         v->next_vertice=NULL;
         v->next_arco=NULL;
+        v->indice_vertice=0;
         strcpy(v->citta, citta);
     }
     return v;
@@ -111,6 +116,7 @@ void inserisci_vertice_in_coda( vertice**testa, vertice*v) // testa<<<<<<
 void nuovo_vertice(grafo*g, char citta[30])
 {
     vertice*v=crea_vertice(citta);
+    v->indice_vertice=g->nv;
     if(!v)
         errore("memoria non disponibile.");
     else
@@ -120,7 +126,25 @@ void nuovo_vertice(grafo*g, char citta[30])
     }
 }
 
+vertice* cerca_vertice(vertice*lista, char citta[30])  // ?????
+{
+    vertice*v=lista;
+    if(v != NULL && strcmp(v->citta, citta) != 0  ) // non trovato
+    {
+        v = cerca_vertice(lista->next_vertice, citta);
+    }
+    return v;
+}
 
+vertice* cerca_vertice_con_indice(vertice*lista, int index)
+{
+    vertice*v=lista;
+    if(v != NULL && v->indice_vertice!=index  ) // non trovato
+    {
+        v = cerca_vertice_con_indice(lista->next_vertice, index);
+    }
+    return v;
+}
 
 
 //********************* ARCO ***********************************
@@ -141,31 +165,23 @@ arco* crea_arco(char citta_arrivo[30], int distanza, float costo)
 
 void inserisci_arco_in_coda(vertice*vert, arco*a)  //  <<<<<< FUNZIONE SBAGLIATA <<<<<
 {
-   if(vert->next_arco == NULL )
-   {
-       vert->next_arco=a;
-   }
-   else
-   {
-       arco*tmp=vert->next_arco;
-       while(tmp->next_arco)
-       {
-           tmp=tmp->next_arco;
-       }
-       tmp->next_arco=a;
-   }
-
-}
-
-vertice* cerca_vertice(vertice*lista, char citta[30])  // ?????
-{
-    vertice*v=lista;
-    if(v != NULL && strcmp(v->citta, citta) != 0  ) // non trovato
+    if(vert->next_arco == NULL )
     {
-        v = cerca_vertice(lista->next_vertice, citta);
+        vert->next_arco=a;
     }
-    return v;
+    else
+    {
+        arco*tmp=vert->next_arco;
+        while(tmp->next_arco)
+        {
+            tmp=tmp->next_arco;
+        }
+        tmp->next_arco=a;
+    }
+
 }
+
+
 
 void nuovo_arco(grafo*g,char citta_partenza[30], char citta_arrivo[30], int distanza, float costo)
 {
@@ -183,14 +199,17 @@ void nuovo_arco(grafo*g,char citta_partenza[30], char citta_arrivo[30], int dist
 
 }
 
-arco* cerca_arco(vertice* v, char citta[]){
+arco* cerca_arco(vertice* v, char citta[])
+{
     arco*tmpA;
 
-    if(v){
+    if(v)
+    {
         tmpA=v->next_arco;
-                while(tmpA)
+        while(tmpA)
         {
-            if(strcmp(tmpA->citta_arrivo,citta)==0){
+            if(strcmp(tmpA->citta_arrivo,citta)==0)
+            {
                 return tmpA;
             }
             tmpA=tmpA->next_arco;
@@ -198,26 +217,31 @@ arco* cerca_arco(vertice* v, char citta[]){
     }
 }
 
+//******************************************************************************+++
+
 //TRATTA ECONOMICA
-char* meta_economica(grafo* g, char citta_partenza[], char* p){
+char* meta_economica(grafo* g, char citta_partenza[], char* p)
+{
 
     int minimo = 1000.0;
     vertice*tmpV=g->lista;
 
     tmpV=cerca_vertice(g->lista,citta_partenza);
-    if(tmpV != NULL){
+    if(tmpV != NULL)
+    {
         arco*tmpA;
         tmpA=tmpV->next_arco;
-            while(tmpA)
+        while(tmpA)
+        {
+            if(minimo > tmpA->costo)
             {
-                if(minimo > tmpA->costo){
-                    minimo = tmpA->costo;
-                    strcpy(p, tmpA->citta_arrivo);
-                }
-                tmpA=tmpA->next_arco;
+                minimo = tmpA->costo;
+                strcpy(p, tmpA->citta_arrivo);
             }
+            tmpA=tmpA->next_arco;
+        }
 
-            return p;
+        return p;
     }
 }
 
@@ -231,7 +255,8 @@ int grado_vertice(grafo*g, char citta[])
         tmpA=tmpV->next_arco;
         while(tmpA)
         {
-            if(strcmp(tmpA->citta_arrivo,citta) == 0){
+            if(strcmp(tmpA->citta_arrivo,citta) == 0)
+            {
                 occorrenze++;
             }
             tmpA=tmpA->next_arco;
@@ -242,8 +267,9 @@ int grado_vertice(grafo*g, char citta[])
     return occorrenze;
 }
 
-/*PRENOTAZIONI*/
-prenotazioni* inizializza_prenotazioni(char citta_partenza[], char citta_arrivo[]) {
+/* **********************PRENOTAZIONI***********************************************/
+prenotazioni* inizializza_prenotazioni(char citta_partenza[], char citta_arrivo[])
+{
     prenotazioni* p = malloc(sizeof(struct prenotazioni));
     strcpy(p->citta_partenza, citta_partenza);
     strcpy(p->citta_arrivo, citta_arrivo);
@@ -251,20 +277,27 @@ prenotazioni* inizializza_prenotazioni(char citta_partenza[], char citta_arrivo[
     return p;
 }
 
-prenotazioni* aggiungi_prenotazione(prenotazioni* p, char citta_partenza[], char citta_arrivo[]){
+prenotazioni* aggiungi_prenotazione(prenotazioni* p, char citta_partenza[], char citta_arrivo[])
+{
 
-    if(p != NULL){
-        if (strcmp(p->citta_partenza, citta_partenza) != 0 || strcmp(p->citta_arrivo, citta_arrivo) != 0){
+    if(p != NULL)
+    {
+        if (strcmp(p->citta_partenza, citta_partenza) != 0 || strcmp(p->citta_arrivo, citta_arrivo) != 0)
+        {
             p->next = aggiungi_prenotazione(p->next, citta_partenza, citta_arrivo);
         }
-    }else{
+    }
+    else
+    {
         p=inizializza_prenotazioni(citta_partenza, citta_arrivo);
     }
     return p;
 }
 
-prenotazioni* testa(prenotazioni* p, char citta_partenza[], char citta_arrivo[]) {
-    if (p != NULL) {
+prenotazioni* testa(prenotazioni* p, char citta_partenza[], char citta_arrivo[])
+{
+    if (p != NULL)
+    {
         prenotazioni* G = malloc(sizeof(struct prenotazioni));
         strcpy(p->citta_partenza, citta_partenza);
         strcpy(p->citta_arrivo, citta_arrivo);
@@ -274,9 +307,12 @@ prenotazioni* testa(prenotazioni* p, char citta_partenza[], char citta_arrivo[])
     return inizializza_prenotazioni(citta_partenza, citta_arrivo);
 }
 
-prenotazioni* rimuovi_prenotazione(prenotazioni* p, char citta_partenza[], char citta_arrivo[]){
-    if (p != NULL) {
-        if (strcmp(p->citta_partenza, citta_partenza) != 0 || strcmp(p->citta_arrivo, citta_arrivo) != 0) {
+prenotazioni* rimuovi_prenotazione(prenotazioni* p, char citta_partenza[], char citta_arrivo[])
+{
+    if (p != NULL)
+    {
+        if (strcmp(p->citta_partenza, citta_partenza) != 0 || strcmp(p->citta_arrivo, citta_arrivo) != 0)
+        {
             prenotazioni* tmp = p->next;
             free(p);
             return tmp;
@@ -286,10 +322,391 @@ prenotazioni* rimuovi_prenotazione(prenotazioni* p, char citta_partenza[], char 
     return p;
 }
 
-void stampa_prenotazioni(prenotazioni* p) {
-    if (p != NULL) {
+void stampa_prenotazioni(prenotazioni* p)
+{
+    if (p != NULL)
+    {
         printf("\ncitta' di partenza: %s\ncitta di arrivo: %s\n", p->citta_partenza, p->citta_arrivo);
         stampa_prenotazioni(p->next);
     }
 }
 
+//*****************************PRENOTAZIONI******************************************
+
+int conferma_login(char utente[])
+{
+
+    int i=0,j=0;
+    FILE *fp;
+    char lettura[20][20];
+    int dim;
+
+    fp=fopen("utenti.txt","r"); //apro il file in lettura
+    if(fp)
+    {
+        while(!feof(fp))
+        {
+
+            fscanf(fp, "%s", lettura[i]);
+            i++;
+
+        }//fine while della lettura file
+
+    }
+    else
+    {
+        printf("Errore apertura file!");
+    }//fine controllo esistenza file
+
+    fclose(fp);
+
+
+    int res = 0;
+    dim = i;
+    for (i = 0; i < dim; i++) //controllo riga per riga se i dati sono presenti nel file
+        if (strcmp(utente, lettura[i]) == 0)
+        {
+            res = 1;
+            return res;
+        }
+
+    return res;
+
+}
+
+void registra_utente(char utente[])
+{
+    FILE *fp;
+    fp=fopen("Utenti.txt","a"); //apro il file
+    if(fp)
+    {
+        fprintf(fp, "%s %d\n", utente, 0);
+    }
+    else
+    {
+        printf("\nErrore nella scrittura del file utenti.txt!\n");
+    }
+    fclose(fp);
+}
+
+void registra_prenotazione(char utente[], char citta_partenza[], char citta_arrivo[])
+{
+    FILE *fp;
+    fp=fopen("prenotazioni.txt","a"); //apro il file
+    if(fp)
+    {
+        fprintf(fp, "%s %s %s\n", utente, citta_partenza, citta_arrivo);
+    }
+    else
+    {
+        printf("\nErrore nella scrittura del file prenotazioni.txt!\n");
+    }
+    fclose(fp);
+}
+
+prenotazioni* attive(char utente[], prenotazioni* p)
+{
+
+    int i=0,j=0;
+    FILE *fp;
+    char lettura[20][20];
+    int dim;
+
+    fp=fopen("prenotazioni.txt","r"); //apro il file in lettura
+    if(fp)
+    {
+        while(!feof(fp))
+        {
+
+            fscanf(fp, "%s", lettura[i]);
+            i++;
+
+        }//fine while della lettura file
+
+    }
+    else
+    {
+        printf("Errore apertura file!");
+    }//fine controllo esistenza file
+
+    fclose(fp);
+
+
+    int res = 0;
+    dim = i;
+    for (i = 0; i < dim; i++)  //controllo riga per riga se i dati sono presenti nel file
+    {
+        if (strcmp(utente, lettura[i]) == 0)
+        {
+            p = aggiungi_prenotazione(p,lettura[i+1],lettura[i+2]);
+        }
+    }
+
+    return p;
+}
+
+
+//**************************************************************************************
+// *************************  FUNZIONI PERCORSO MINIMO ********************************
+
+#define INFINITO 999999
+
+//**************************  per il COSTO ***************************************
+
+int get_indice_prossimo_vertice_per_costo(float pesi[], int visitati[],  int lunghezza_array)  //*** trova indce pross vert
+{
+    int min_index=-1;
+    for(int i=0; i<lunghezza_array; i++)
+    {
+        if(visitati[i] == 0)
+        {
+            if(min_index == -1 )
+            {
+                min_index=i;
+                continue;
+            }
+            if(pesi[i] < pesi[min_index])
+            {
+                min_index=i;
+            }
+        }
+    }
+    return min_index;
+}
+
+void rilassa_adiacenti_per_costo(grafo*g, vertice* v, float pesi[], int visitati[]) // OK FUNZIONA
+{
+    arco*tmpA=v->next_arco;
+    visitati[v->indice_vertice]=1;
+    vertice*tmpV;
+
+    while(tmpA)
+    {
+        tmpV=cerca_vertice(g->lista,tmpA->citta_arrivo );
+        if(  pesi[tmpV->indice_vertice]  > tmpA->costo + pesi[v->indice_vertice])
+            pesi[tmpV->indice_vertice]  = tmpA->costo + pesi[v->indice_vertice];
+
+        tmpA=tmpA->next_arco;
+    }
+
+    int index=get_indice_prossimo_vertice_per_costo(pesi, visitati, g->nv);
+    if(index<0)
+        return;
+
+    vertice*prox_vert=cerca_vertice_con_indice(g->lista, index);
+    rilassa_adiacenti_per_costo(g, prox_vert, pesi, visitati);
+}
+
+
+float dijkstra_sp_costo(grafo*g, char citta_partenza[], char citta_arrivo[])
+{
+    float pesi[g->nv];
+    int visitati[g->nv];
+    float risultato;
+    vertice*v_part=cerca_vertice(g->lista, citta_partenza);
+    vertice*v_arr=cerca_vertice(g->lista, citta_arrivo);
+
+    if(!v_part || !v_arr)
+        return -2;
+
+    for(int i=0; i<g->nv; i++)
+    {
+        pesi[i]=INFINITO;
+        visitati[i]=0;
+    }
+    pesi[v_part->indice_vertice]=0;
+    visitati[v_part->indice_vertice]=1;
+
+    rilassa_adiacenti_per_costo(g, v_part, pesi, visitati);
+
+    if(pesi[v_arr->indice_vertice] == INFINITO)
+        risultato = -1;
+    else
+        risultato = pesi[v_arr->indice_vertice];
+
+    return risultato;
+}
+
+//*************************** per la DISTANZA  ********************************************
+
+int get_indice_prossimo_vertice_per_distanza(int pesi[], int visitati[],  int lunghezza_array)  //*** trova indce pross vert
+{
+    int min_index=-1;
+    for(int i=0; i<lunghezza_array; i++)
+    {
+        if(visitati[i] == 0)
+        {
+            if(min_index == -1 )
+            {
+                min_index=i;
+                continue;
+            }
+            if(pesi[i] < pesi[min_index])
+            {
+                min_index=i;
+            }
+        }
+    }
+    return min_index;
+}
+
+
+void rilassa_adiacenti_per_distanza(grafo*g, vertice* v, int pesi[], int visitati[]) // OK FUNZIONA
+{
+    arco*tmpA=v->next_arco;
+    visitati[v->indice_vertice]=1;
+    vertice*tmpV;
+
+    while(tmpA)
+    {
+        tmpV=cerca_vertice(g->lista,tmpA->citta_arrivo );
+        if(  pesi[tmpV->indice_vertice]  > tmpA->distanza + pesi[v->indice_vertice])
+            pesi[tmpV->indice_vertice]  = tmpA->distanza + pesi[v->indice_vertice];
+
+        tmpA=tmpA->next_arco;
+    }
+
+    int index=get_indice_prossimo_vertice_per_distanza(pesi, visitati, g->nv);
+    if(index<0)
+        return;
+
+    vertice*prox_vert=cerca_vertice_con_indice(g->lista, index);
+    rilassa_adiacenti_per_distanza(g, prox_vert, pesi, visitati);
+}
+
+
+int dijkstra_sp_distanza(grafo*g, char citta_partenza[], char citta_arrivo[])
+{
+    int pesi[g->nv];
+    int visitati[g->nv];
+    int risultato;
+    vertice*v_part=cerca_vertice(g->lista, citta_partenza);
+    vertice*v_arr=cerca_vertice(g->lista, citta_arrivo);
+
+    if(!v_part || !v_arr)
+        return -2;
+
+    for(int i=0; i<g->nv; i++)
+    {
+        pesi[i]=INFINITO;
+        visitati[i]=0;
+    }
+    pesi[v_part->indice_vertice]=0;
+    visitati[v_part->indice_vertice]=1;
+
+    rilassa_adiacenti_per_distanza(g, v_part, pesi, visitati);
+
+    if(pesi[v_arr->indice_vertice] == INFINITO)
+        risultato = -1;
+    else
+        risultato = pesi[v_arr->indice_vertice];
+
+    return risultato;
+}
+
+
+//********************************* FILE ***************************************
+void leggi_file_vertici(grafo*g)
+{
+    char citta[30];
+    FILE*fp=fopen("Vertici.txt", "r");
+    if(!fp)
+        errore("impossibile aprire il file vertici.");
+
+    while(!feof(fp))
+    {
+        fscanf(fp, "%30s\n", &citta);
+        nuovo_vertice(g, citta);
+    }
+    fclose(fp);
+}
+
+void leggi_file_archi(grafo*g)
+{
+    int distanza;
+    float costo;
+    char citta_partenza[30], citta_arrivo[30];
+    FILE*fp=fopen("Archi.txt", "r");
+    if(!fp)
+        errore("impossibile aprire il file archi.");
+    while(!feof(fp))
+    {
+        fscanf(fp, "%30s %30s %f %d\n", citta_partenza,citta_arrivo, &costo, &distanza );
+        nuovo_arco(g, citta_partenza, citta_arrivo, distanza, costo );
+    }
+    fclose(fp);
+}
+
+
+//******************************* FUNZIONI UTILI  *********************************
+
+void stampa_menu()
+{
+    printf("\nSELEZIONA UN OPZIONE\n");
+    printf("\n1.Visualizza i voli");
+    printf("\n2.Visualizza le prenotazioni attive");
+    printf("\n3.Effettua una nuova prenotazione");
+    printf("\n4.ESCI\n");
+}
+
+void inizializza(grafo* g)
+{
+    leggi_file_vertici(g);
+    leggi_file_archi(g);
+}
+
+void continua()
+{
+    system("PAUSE");
+    system("cls");
+}
+
+char* normalizza_parola(char* p)
+{
+
+    p[0] = toupper(p[0]);
+    for(int i=1; i<strlen(p); i++)
+    {
+        p[i] = tolower(p[i]);
+    }
+
+    return p;
+}
+
+int richiedi_intero()
+{
+    int valore;
+    while(scanf("%d", &valore)!= 1)
+    {
+        printf("\nValore non valido. Inserirne un altro.");
+        fflush(stdin);
+    }
+    return valore;
+}
+
+void richiedi_nome_citta(char nome[])
+{
+    printf("\n> ");
+    scanf("%29s",nome );
+    normalizza_parola(nome);
+}
+
+char richiedi_Y_N()
+{
+    char scelta;
+    do
+    {
+        fflush(stdin);
+        printf("\n\nY=yes\nN=no\n> ");
+        scelta=getchar();
+        scelta=toupper(scelta);
+        if(scelta!='Y' && scelta!='N')
+            printf("Non valido.");
+
+    }
+    while(scelta!='Y' && scelta!='N');
+
+    return scelta;
+}
+
+//***********************************************************************************
--- Progetto/main.c
+++ Progetto/main.c
@@ -4,10 +4,10 @@
 #include<ctype.h>
 #include"header.h"
 
-grafo* inizializza(grafo*g); //riempe il grafo
-void continua(); //chiede di premere un tasto per continuare
-char* normalizza_parola(char* p); //rende la prima lettera maiuscola e le restanti minuscole
-prenotazioni* attive(char utente[], prenotazioni* p);
+//grafo* inizializza(grafo*g); //riempe il grafo
+//void continua(); //chiede di premere un tasto per continuare
+//char* normalizza_parola(char* p); //rende la prima lettera maiuscola e le restanti minuscole
+//prenotazioni* attive(char utente[], prenotazioni* p);
 
 void main()
 {
@@ -27,7 +27,7 @@ void main()
     grafo*g=nuovo_grafo();
     prenotazioni* lista_prenotazioni = NULL;
 
-    g=inizializza(g);
+    inizializza(g);
 
 while(confermato != 1){
     printf("\nLOGIN");
@@ -162,74 +162,8 @@ if(scelta_login == 3){
 } //fine if-else
 }
 
-grafo* inizializza(grafo* g){
-    nuovo_vertice(g, "Napoli");
-    nuovo_vertice(g, "Roma");
-    nuovo_vertice(g, "Milano");
-    nuovo_vertice(g, "Verona");
-    nuovo_vertice(g, "Barcellona");
-    nuovo_vertice(g, "Madrid");
-    nuovo_vertice(g, "Amsterdam");
-    nuovo_vertice(g, "Pisa");
-    nuovo_vertice(g, "Parigi");
-    nuovo_vertice(g, "Londra");
-    nuovo_vertice(g, "Bari");
-    nuovo_vertice(g, "Piacenza");
-    nuovo_vertice(g, "Mosca");
-    nuovo_vertice(g, "Bruxelles");
-    nuovo_vertice(g, "Helsinki");
-    nuovo_vertice(g, "Tokyo");
-    nuovo_vertice(g, "Montecarlo");
-    nuovo_vertice(g, "Edimburgo");
-    nuovo_vertice(g, "Stoccolma");
-    nuovo_vertice(g, "Dublino");
 
-    nuovo_arco(g, "Napoli", "Roma",   200, 20.0);
-    nuovo_arco(g, "Roma", "Napoli",   200, 20.0);
-    nuovo_arco(g, "Napoli", "Milano", 700, 50.0);
-    nuovo_arco(g, "Milano", "Napoli",   700, 20.0);
-    nuovo_arco(g, "Verona", "Barcellona", 1100, 100.0);
-    nuovo_arco(g, "Barcellona", "Verona", 1100, 100.0);
-    nuovo_arco(g, "Helsinki", "Montecarlo",   2800, 200.0);
-    nuovo_arco(g, "Montecarlo", "Helsinki",   2800, 200.0);
-    nuovo_arco(g, "Bruxelles", "Helsinki", 700, 50.0);
-    nuovo_arco(g, "Helsinki", "Bruxelles", 700, 50.0);
-    nuovo_arco(g, "Mosca", "Tokyo", 7500, 100.0);
-    nuovo_arco(g, "Tokyo", "Mosca", 7500, 100.0);
-    nuovo_arco(g, "Napoli", "Barcellona",   1500, 80.0);
-    nuovo_arco(g, "Barcellona", "Napoli",   1500, 80.0);
-    nuovo_arco(g, "Amsterdam", "Milano", 1070, 90.0);
-    nuovo_arco(g, "Milano", "Amsterdam", 1070, 90.0);
-    nuovo_arco(g, "Milano", "Bruxelles", 800, 50.0);
-    nuovo_arco(g, "Bruxelles", "Milano", 800, 50.0);
-    nuovo_arco(g, "Napoli", "Dublino", 2600, 150.0);
-    nuovo_arco(g, "Dublino", "Napoli", 2600, 150.0);
-    nuovo_arco(g, "Stoccolma", "Edimburgo", 2500, 180.0);
-    nuovo_arco(g, "Edimburgo", "Stoccolma", 2500, 180.0);
-    nuovo_arco(g, "Londra", "Parigi", 470, 50.0);
-    nuovo_arco(g, "Parigi", "Londra", 470, 50.0);
-    nuovo_arco(g, "Londra", "Napoli", 2000, 100.0);
-    nuovo_arco(g, "Napoli", "Londra", 2000, 100.0);
-
-    return g;
-}
-
-void continua(){
-    char tasto;
-    printf("\n\nPremere un tasto per continuare...\n");
-    scanf("%s",&tasto);
-    system("cls");
-}
-
-char* normalizza_parola(char* p){
-
-    p[0] = toupper(p[0]);
-    for(int i=1; i<strlen(p); i++){
-        p[i] = tolower(p[i]);
-    }
-
-    return p;
-}
+/*
 
 int conferma_login(char utente[]){
 
@@ -321,3 +255,5 @@ prenotazioni* attive(char utente[], prenotazioni* p){
 
     return p;
 }
+
+*/

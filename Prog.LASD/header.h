ef882e2df399abfbcb81b9f0d27c789c3575e473
antonio <57714209+antov93@users.noreply.github.com>
2020-06-11 17:43:57 +0200

    Aggiunta stampa voli durante la prenotazione.

--- Prog.LASD/header.h
+++ Prog.LASD/header.h
@@ -514,7 +514,7 @@ arco* meta_piu_econimica(grafo*g, char citta_partenza[])
 char*meta_piu_gettonata(grafo*g, prenotazione*lista_prenotazioni)
 {
     int num_prenotazioni[g->nv];
-    int max=0; // indice della citta più gettonata
+    int max=0; // indice della citta piÃ¹ gettonata
     prenotazione*tmp = lista_prenotazioni;
     vertice*v;
 
@@ -527,7 +527,7 @@ char*meta_piu_gettonata(grafo*g, prenotazione*lista_prenotazioni)
     for(int i=0; i<g->nv; i++) // inizializza le celle del vettore a 0
         num_prenotazioni[i]=0;
 
-    while(tmp) // scorre tutte le prenotazioni e in base all'indice di ogni città destinazione aumenta di uno la cella del vettore
+    while(tmp) // scorre tutte le prenotazioni e in base all'indice di ogni cittÃ  destinazione aumenta di uno la cella del vettore
     {
         v=cerca_vertice(g->lista, tmp->citta_arrivo);
         num_prenotazioni[v->indice_vertice]++;
@@ -1074,6 +1074,7 @@ void visualizza_opzioni_voli(grafo*g, utente*u, prenotazione**lista_prenotazioni
 
 
     pulisci_schermo();
+    stampa_grafo(g);
     printf("Inserire citta' partenza : ");
     richiedi_nome_citta(partenza);
     if(cerca_vertice(g->lista, partenza)==NULL)
